42b7b9195c6bf246bd7efa87d95e9001242b3723
atchim <atchim.gama.silva@gmail.com>
2024-03-02 23:41:22 -0300

    fix(plugins/null-ls): remove unsupported eslint_d code action

--- lua/uma-dlc/plugins/null-ls.fnl
+++ lua/uma-dlc/plugins/null-ls.fnl
@@ -3,6 +3,5 @@
   :opts
   (fn []
     (let [nls (require :null-ls) builtins nls.builtins]
-      { :sources
-        [builtins.code_actions.eslint_d builtins.formatting.prettierd]}))
+      {:sources [builtins.formatting.prettierd]}))
   :dependencies :nvim-lua/plenary.nvim}
--- lua/uma-dlc/plugins/null-ls.lua
+++ lua/uma-dlc/plugins/null-ls.lua
@@ -1,6 +1,6 @@
 local function _1_()
   local nls = require("null-ls")
   local builtins = nls.builtins
-  return {sources = {builtins.code_actions.eslint_d, builtins.formatting.prettierd}}
+  return {sources = {builtins.formatting.prettierd}}
 end
 return {"nvimtools/none-ls.nvim", event = {"BufNew", "BufNewFile", "BufReadPre"}, opts = _1_, dependencies = "nvim-lua/plenary.nvim"}
